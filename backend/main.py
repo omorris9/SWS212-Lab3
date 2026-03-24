f34f832eb99b47c84fe8f17f2576e72d1318454c
Olivia <omorris9@my.centennialcollege.ca>
2026-03-24 01:55:07 -0400

    Update

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://lab3-dv4i.onrender.com",
+        "https://sws212-lab3.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
